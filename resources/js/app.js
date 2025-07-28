5e1a176cc1f942b35ecef1fc6e076afdb9084c6e
Nhan Vo <>
2025-07-28 22:57:42 +0700

    Refactor: Optimize frontend assets and improve code structure

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,17 @@
 import './bootstrap';
 
+import TomSelect from 'tom-select';
 import 'tom-select/dist/css/tom-select.css';
 import '../css/components/tom-select.css';
 
 import Alpine from 'alpinejs';
-import TomSelect from 'tom-select';
-
-// Import logic cho component live-table
 import liveTable from './components/admin/liveTable';
 import permissionGroup from './components/admin/permissionGroups';
 
 window.TomSelect = TomSelect;
 window.liveTable = liveTable;
+window.permissionGroup = permissionGroup;
 
 window.Alpine = Alpine;
 
-Alpine.data('permissionGroup', permissionGroup);
 Alpine.start();
